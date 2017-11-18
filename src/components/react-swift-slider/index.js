f1844024ffa3b49cf2282e731ea9083e0fbeebdf
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2017-11-18 18:34:49 +0200

    [muzi] refactor slider :boom:

--- src/components/react-swift-slider/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Muzikayise Flynn Buthelezi (zuluCoda) on 2017/11/18.
- * Copyright mfbproject.co.za - muzi@mfbproject.co.za
- * Copyright zulucoda - mfbproject
- */
-export Slider from "./Slider";
