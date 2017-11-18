1d099ba5d70630a728e0e42e4cbaebe32f0a2642
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2017-11-18 15:04:48 +0200

    [muzi] adding enzyme for test :fire:

---
+++ config/jest/test-setup.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Muzikayise Flynn Buthelezi (zuluCoda) on 2017/11/18.
+ * Copyright mfbproject.co.za - muzi@mfbproject.co.za
+ * Copyright zulucoda - mfbproject
+ */
+// test-setup.js
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
---
+++ src/components/react-swift-slider/tests/slider.spec.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Muzikayise Flynn Buthelezi (zuluCoda) on 2017/11/18.
+ * Copyright mfbproject.co.za - muzi@mfbproject.co.za
+ * Copyright zulucoda - mfbproject
+ */
