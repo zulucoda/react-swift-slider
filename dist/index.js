bd358621d728435b14986c66e07b9238a01c915c
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2017-11-18 13:29:33 +0200

    [muzi] updating to react 16 and using create-react-app for package :boom: :fire:

--- dist/index.js
+++
@@ -1,2 +0,0 @@
-const Slider = require('./Slider');
-module.exports = Slider;
--- postcss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = () => [
-    require('postcss-cssnext')({
-      browsers: 'last 2 versions'
-    }),
-  ]
