8e4c659e1dad6120fde8fdd41b35cd7373ad725b
Muzikayise Flynn Buthelezi (zulucoda) <muzi@mfbproject.co.za>
2019-06-23 07:47:58 +0200

    updating slide.spec to use @testing-library/react :boom: :boom:

--- src/react-swift-slider/components/tests/slide.spec.js
+++ src/react-swift-slider/components/tests/slide.spec.js
@@ -4,35 +4,36 @@
  * Copyright zulucoda - mfbproject
  */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import Slide from '../Slide';
 
 describe('React Swift Slider: Slide - Unit Test', () => {
   describe('when active', () => {
-    it('should include ".swift-slider-active" class name', () => {
+    it('should render Slide with active css properties', () => {
       const props = {
         active: true,
         src: 'some src url',
       };
 
-      const wrapper = shallow(<Slide src={props.src} active={props.active} />);
+      const { container } = render(
+        <Slide src={props.src} active={props.active} />,
+      );
 
-      expect(wrapper.find('.swift-slider-active').length).toEqual(1);
+      expect(container.firstChild).toMatchSnapshot();
     });
   });
   describe('when NOT active', () => {
-    it('should NOT include ".swift-slider-active" class name', () => {
+    it('should render Slide without active css properties', () => {
       const props = {
         active: false,
         src: 'some src url',
       };
 
-      const wrapper = shallow(<Slide src={props.src} active={props.active} />);
-
-      expect(wrapper.find('li').length).toEqual(1);
-      expect(wrapper.find('li').getElement().props.className).toEqual(
-        'swift-slider-slide',
+      const { container } = render(
+        <Slide src={props.src} active={props.active} />,
       );
+
+      expect(container.firstChild).toMatchSnapshot();
     });
   });
 });
