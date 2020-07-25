066663328be911c2abf18b71d93261b71e2aa470
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-07-25 21:42:45 +0200

    adding webpack config

---
+++ example/typescript/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './app/main.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+  },
+  devServer: {
+    inline: false,
+  },
+};
