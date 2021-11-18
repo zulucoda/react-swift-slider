07980e5607165e22590ad73a53779b5845f994e6
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2021-11-18 15:09:47 +0200

    fix: updating rollup to latest and updating @rollup to use new plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 // rollup.config.js
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
